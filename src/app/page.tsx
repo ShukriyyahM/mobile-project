79afe60e60aba54247679450cacd56435c1613cd
ShukriyyahM <ishaqshakeerah@gmail.com>
2026-04-29 13:01:16 +0100

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,7 @@ export default function Home() {
       
       <section id="contact">
         <footer className="bg-green-950 text-white text-center py-6">
-          © Notzero2026 AviFlu Monitoring System. All rights reserved.
+          © 2026 AviFlu Monitoring System. All rights reserved.
         </footer>
       </section>
 
